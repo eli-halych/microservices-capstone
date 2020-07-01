3fd9e23ae82ac02947cf0b65c847434326f1fe72
Illia Halych <eli.halych@gmail.com>
2020-07-01 15:40:03 +0200

    [ADD]: API Gateway update utils + logger

---
+++ backend/src/lambda/utils.ts
@@ -0,0 +1,20 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { parseUserId } from "../auth/utils";
+import {createLogger} from "../utils/logger";
+
+/**
+ * Get a user id from an API Gateway event
+ * @param event an event from API Gateway
+ *
+ * @returns a user id from a JWT token
+ */
+
+const logger = createLogger('api-gateway-utils');
+export function getUserId(event: APIGatewayProxyEvent): string {
+  logger.info('Extracting user id from JWT');
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(' ');
+  const jwtToken = split[1];
+
+  return parseUserId(jwtToken)
+}

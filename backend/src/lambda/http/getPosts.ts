a65909198d321755981a81f682ecb2ffd5e2051c
Illia Halych <eli.halych@gmail.com>
2020-07-01 15:32:57 +0200

    [ADD]: GET image post Lambda handler

---
+++ backend/src/lambda/http/getPosts.ts
@@ -0,0 +1,18 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const userId = getUserId(event);
+    const todos = await getAllImagePosts(userId);
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            items: todos
+        })
+    }
+}

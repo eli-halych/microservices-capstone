53b6bf99091d775f49ecdcad94a77a54c0eaaec0
Illia Halych <eli.halych@gmail.com>
2020-07-01 16:20:49 +0200

    [ADD]: UPDATE request structure and types
    - name, location, datePosted, description

---
+++ backend/src/requests/UpdatePostImageRequest.ts
@@ -0,0 +1,9 @@
+/**
+ * Fields in a request to update a single image post.
+ */
+export interface UpdateTodoRequest {
+    name: string
+    location: string
+    datePosted: string
+    description: string
+}
